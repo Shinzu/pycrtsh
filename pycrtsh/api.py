5c8190461e4ac3c3ef7feabdb7f8225776164db9
Markus Wyrsch <markus.wyrsch@zalando.de>
2020-06-26 17:10:57 +0200

    Enable and fix several pre-commit hooks

--- pycrtsh/api.py
+++ pycrtsh/api.py
@@ -1,153 +1,191 @@
-import requests
+"""Lib to query crt.sh."""
 import re
+
+import requests
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 
 
 class CrtshInvalidRequestType(Exception):
-    """Exception if the request is invalid"""
+
+    """Exception if the request is invalid."""
+
     def __init__(self):
         Exception.__init__(self, "Invalid request type")
 
 
 class CrtshCertificateNotFound(Exception):
-    """Exception if a certificate is not found"""
+
+    """Exception if a certificate is not found."""
+
     def __init__(self):
         Exception.__init__(self, "Certificate not found")
 
 
-class Crtsh(object):
-    """
-    Main Crtsh object
-    """
+class Crtsh:
+
+    """Main Crtsh object."""
+
     def __init__(self):
         pass
 
-    def search(self, query, icaid=None, exclude=None):
+    @staticmethod
+    def search(query, icaid=None, exclude=None):
         """
-        Search crt.sh with the give query
-        Query can be domain, sha1, sha256...
+        Search crt.sh with the give query Query.
+
+        can be domain, sha1, sha256.
         """
         if exclude is not None:
-            r = requests.get('https://crt.sh/', params={'q': query, 'output': 'json', 'exclude': exclude})
+            res = requests.get(
+                "https://crt.sh/",
+                params={"q": query, "output": "json", "exclude": exclude},
+            )
         if exclude is None:
-            r = requests.get('https://crt.sh/', params={'q': query, 'output': 'json'})
-        nameparser = re.compile("([a-zA-Z]+)=(\"[^\"]+\"|[^,]+)")
+            res = requests.get(
+                "https://crt.sh/", params={"q": query, "output": "json"},
+            )
+        nameparser = re.compile('([a-zA-Z]+)=("[^"]+"|[^,]+)')
         certs = []
-        for c in r.json():
-            if icaid is not None and c['issuer_ca_id'] == int(icaid):
-                certs.append({
-                    'id': c['id'],
-                    'logged_at': parse(c['entry_timestamp']),
-                    'not_before': parse(c['not_before']),
-                    'not_after': parse(c['not_after']),
-                    'name': c['name_value'],
-                    'ca': {
-                        'caid': c['issuer_ca_id'],
-                        'name': c['issuer_name'],
-                        'parsed_name': dict(nameparser.findall(c['issuer_name']))
+        for cert in res.json():
+            if icaid is not None and cert["issuer_ca_id"] == int(icaid):
+                certs.append(
+                    {
+                        "id": cert["id"],
+                        "logged_at": parse(cert["entry_timestamp"]),
+                        "not_before": parse(cert["not_before"]),
+                        "not_after": parse(cert["not_after"]),
+                        "name": cert["name_value"],
+                        "ca": {
+                            "caid": cert["issuer_ca_id"],
+                            "name": cert["issuer_name"],
+                            "parsed_name": dict(
+                                nameparser.findall(cert["issuer_name"])
+                            ),
+                        },
                     }
-                })
+                )
             if icaid is None:
-                certs.append({
-                    'id': c['id'],
-                    'logged_at': parse(c['entry_timestamp']),
-                    'not_before': parse(c['not_before']),
-                    'not_after': parse(c['not_after']),
-                    'name': c['name_value'],
-                    'ca': {
-                        'caid': c['issuer_ca_id'],
-                        'name': c['issuer_name'],
-                        'parsed_name': dict(nameparser.findall(c['issuer_name']))
+                certs.append(
+                    {
+                        "id": cert["id"],
+                        "logged_at": parse(cert["entry_timestamp"]),
+                        "not_before": parse(cert["not_before"]),
+                        "not_after": parse(cert["not_after"]),
+                        "name": cert["name_value"],
+                        "ca": {
+                            "caid": cert["issuer_ca_id"],
+                            "name": cert["issuer_name"],
+                            "parsed_name": dict(
+                                nameparser.findall(cert["issuer_name"])
+                            ),
+                        },
                     }
-                })
+                )
 
         return certs
 
-    def get(self, query, type="sha1"):
+    @staticmethod
+    def get(query, ctype="sha1"):
         """
-        Search for a certificate with the given value of the given type
-        value can be either a crtsh id, sha1 or sha256
-        type has to be in ['id', 'sha1', 'sha256']
+        Search for a certificate with the given value certificate type.
+
+        can be either a crtsh id, sha1 or sha256.
         """
-        if type not in ["sha1", "sha256", "id"]:
+        # pylint: disable=too-many-branches,too-many-statements
+        if ctype not in ["sha1", "sha256", "id"]:
             raise CrtshInvalidRequestType()
-        if type == "id":
-            r = requests.get('https://crt.sh/', params={'id': query})
+        if ctype == "id":
+            res = requests.get("https://crt.sh/", params={"id": query})
         else:
-            r = requests.get('https://crt.sh/', params={'q': query})
+            res = requests.get("https://crt.sh/", params={"q": query})
 
-        if "<BR><BR>Certificate not found </BODY>" in r.text:
+        if "<BR><BR>Certificate not found </BODY>" in res.text:
             raise CrtshCertificateNotFound()
-        if "<BR><BR>Invalid value:" in r.text:
+        if "<BR><BR>Invalid value:" in res.text:
             raise CrtshCertificateNotFound()
 
-        soup = BeautifulSoup(r.text, 'lxml')
-        table = soup.find_all('table')[1]
+        soup = BeautifulSoup(res.text, "lxml")
+        table = soup.find_all("table")[1]
         cert = {}
-        lines1 = table.find_all('tr', recursive=False)
+        lines1 = table.find_all("tr", recursive=False)
         if len(lines1) < 7:
-            # It means that we are in a research not in a certificate description
+            # It means that we are in a research
+            # not in a certificate description
             # ie https://crt.sh/?q=sha1
             raise CrtshCertificateNotFound()
 
-        cert['id'] = lines1[0].td.text
-        cert['summary'] = lines1[1].td.text
-        cert['sha256'] = lines1[4].a.text
-        cert['sha1'] = lines1[5].td.text
-        certinfo = str(lines1[6].td)[60:-6].split('<br/>')
+        cert["id"] = lines1[0].td.text
+        cert["summary"] = lines1[1].td.text
+        cert["sha256"] = lines1[4].a.text
+        cert["sha1"] = lines1[5].td.text
+        certinfo = str(lines1[6].td)[60:-6].split("<br/>")
         i = 0
         while i < len(certinfo):
             if "Version:" in certinfo[i]:
-                cert['version'] = certinfo[i].strip().split("\xa0")[1]
+                cert["version"] = certinfo[i].strip().split("\xa0")[1]
             if "Serial\xa0Number:" in certinfo[i]:
                 # Size of serial may change
                 ends = certinfo[i][25:].find('"')
-                cert['serial'] = certinfo[i][25:25+ends]
+                cert["serial"] = certinfo[i][25 : 25 + ends]  # noqa: E203
             if "Signature\xa0Algorithm:" in certinfo[i]:
-                if 'signature_algorithm' in cert.keys():
+                if "signature_algorithm" in cert.keys():
                     signature = ""
                     i += 1
-                    while certinfo[i].startswith("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"):
+                    while certinfo[i].startswith(
+                        "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"
+                    ):
                         signature += certinfo[i][9:]
                         i += 1
                     i -= 1
-                    cert['signature'] = signature.replace(":", "")
+                    cert["signature"] = signature.replace(":", "")
                 else:
-                    cert['signature_algorithm'] = certinfo[i].split(":")[1].strip()
+                    cert["signature_algorithm"] = certinfo[i].split(":")[1].strip()
             if ">Issuer:</a>" in certinfo[i]:
-                cert['issuer'] = {'id': certinfo[i].split('"')[1][6:]}
+                cert["issuer"] = {"id": certinfo[i].split('"')[1][6:]}
                 i += 1
-                while certinfo[i].startswith('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'):
+                while certinfo[i].startswith(
+                    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"
+                ):
                     split = certinfo[i].split("=")
-                    cert['issuer'][split[0].strip()] = split[1].strip().replace('\xa0', ' ')
+                    cert["issuer"][split[0].strip()] = (
+                        split[1].strip().replace("\xa0", " ")
+                    )
                     i += 1
                 i -= 1
             if "\xa0Not\xa0Before:" in certinfo[i]:
-                cert['not_before'] = parse(certinfo[i][24:])
+                cert["not_before"] = parse(certinfo[i][24:])
             if "\xa0Not\xa0After\xa0:" in certinfo[i]:
-                cert['not_after'] = parse(certinfo[i][24:])
+                cert["not_after"] = parse(certinfo[i][24:])
             if "\xa0Subject:" in certinfo[i]:
-                cert['subject'] = {}
+                cert["subject"] = {}
                 i += 1
-                while certinfo[i].startswith('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'):
+                while certinfo[i].startswith(
+                    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"
+                ):
                     split = certinfo[i].split("=")
-                    cert['subject'][split[0].strip()] = split[1].strip().replace('\xa0', ' ')
+                    cert["subject"][split[0].strip()] = (
+                        split[1].strip().replace("\xa0", " ")
+                    )
                     i += 1
                 i -= 1
             if "Subject\xa0Public\xa0Key\xa0Info:</a>" in certinfo[i]:
-                cert["publickey"] = {'sha256': certinfo[i].split("=")[2][:64]}
+                cert["publickey"] = {"sha256": certinfo[i].split("=")[2][:64]}
             if "Public\xa0Key\xa0Algorithm" in certinfo[i]:
                 cert["publickey"]["algorithm"] = certinfo[i].split(":")[1].strip()
             if "\xa0Public-Key:\xa0(" in certinfo[i]:
-                cert["publickey"]["size"] = int(certinfo[i].split("(")[1].split("\xa0")[0])
+                cert["publickey"]["size"] = int(
+                    certinfo[i].split("(")[1].split("\xa0")[0]
+                )
             if "\xa0Modulus:" in certinfo[i]:
                 modulus = ""
                 i += 1
-                while certinfo[i].startswith("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"):
+                while certinfo[i].startswith(
+                    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"  # noqa: E501 pylint: disable=line-too-long
+                ):
                     modulus += certinfo[i][20:]
                     i += 1
-                cert['publickey']['modulus'] = modulus.replace(":", "")
+                cert["publickey"]["modulus"] = modulus.replace(":", "")
                 i -= 1
             if "Exponent:" in certinfo[i]:
                 cert["publickey"]["exponent"] = certinfo[i][26:].split("\xa0")[0]
@@ -156,37 +194,57 @@ class Crtsh(object):
             if "\xa0Subject\xa0Alternative\xa0Name:" in certinfo[i]:
                 cert["extensions"]["alternative_names"] = []
                 i += 1
-                while certinfo[i].startswith("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0DNS:"):
-                    cert["extensions"]["alternative_names"].append(certinfo[i][20:].strip())
+                while certinfo[i].startswith(
+                    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0DNS:"  # noqa: E501 pylint: disable=line-too-long
+                ):
+                    cert["extensions"]["alternative_names"].append(
+                        certinfo[i][20:].strip()
+                    )
                     i += 1
                 i -= 1
             if "X509v3\xa0Basic\xa0Constraints:\xa0" in certinfo[i]:
                 i += 1
-                cert["extensions"]["basic_constraints"] = ("CA:FALSE" not in certinfo[i])
+                cert["extensions"]["basic_constraints"] = "CA:FALSE" not in certinfo[i]
             if "X509v3\xa0Key\xa0Usage:" in certinfo[i]:
                 cert["extensions"]["key_usage"] = {
                     "critical": ("Usage:\xa0critical" in certinfo[i])
                 }
                 i += 1
-                cert["extensions"]["key_usage"]["usage"] = [a.strip().replace("\xa0", " ") for a in certinfo[i].split(",")]
+                cert["extensions"]["key_usage"]["usage"] = [
+                    a.strip().replace("\xa0", " ") for a in certinfo[i].split(",")
+                ]
             if "X509v3\xa0CRL\xa0Distribution\xa0Points:" in certinfo[i]:
                 i += 3
-                cert["extensions"]["crl_distribution"] = {"url": certinfo[i].split("URI:")[1].strip()}
+                cert["extensions"]["crl_distribution"] = {
+                    "url": certinfo[i].split("URI:")[1].strip()
+                }
             if "X509v3\xa0Extended\xa0Key\xa0Usage:" in certinfo[i]:
                 i += 1
-                cert["extensions"]["extended_key_usage"] = {"usage": [a.strip().replace("\xa0", " ") for a in certinfo[i].split(",")]}
+                cert["extensions"]["extended_key_usage"] = {
+                    "usage": [
+                        a.strip().replace("\xa0", " ") for a in certinfo[i].split(",")
+                    ]
+                }
             if "X509v3\xa0Authority\xa0Key\xa0Identifier:" in certinfo[i]:
                 i += 1
-                cert["extensions"]["authority_key_identifier"] = certinfo[i][22:].replace(":", "")
+                cert["extensions"]["authority_key_identifier"] = certinfo[i][
+                    22:
+                ].replace(":", "")
             if "X509v3\xa0Subject\xa0Key\xa0Identifier:" in certinfo[i]:
                 i += 1
-                cert["extensions"]["subject_key_identifier"] = certinfo[i][16:].replace(":", "")
+                cert["extensions"]["subject_key_identifier"] = certinfo[i][16:].replace(
+                    ":", ""
+                )
             if "Authority\xa0Information\xa0Access:" in certinfo[i]:
                 cert["extensions"]["authority_information_access"] = {}
                 i += 1
-                while certinfo[i].startswith("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"):
+                while certinfo[i].startswith(
+                    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"
+                ):
                     split = certinfo[i].split("\xa0-\xa0")
-                    cert["extensions"]["authority_information_access"][split[0].strip().replace("\xa0", " ")] = split[1].strip()
+                    cert["extensions"]["authority_information_access"][
+                        split[0].strip().replace("\xa0", " ")
+                    ] = split[1].strip()
                     i += 1
                 i -= 1
             # Warning : does not parse all the X509 extensions
--- pycrtsh/cli.py
+++ pycrtsh/cli.py
@@ -1,40 +1,63 @@
+"""CLI for pycrtsh api functions."""
 import argparse
-import json
 import datetime
+import json
+
 from prettytable import PrettyTable
+
 from .api import Crtsh
 
 
-def datetime_handler(x):
-    if isinstance(x, datetime.datetime):
-        return x.isoformat()
+def datetime_handler(dtime):
+    """Convert datetime object."""
+    if isinstance(dtime, datetime.datetime):
+        return dtime.isoformat()
     raise TypeError("Unknown type")
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Request crt.sh')
-    subparsers = parser.add_subparsers(help='Commands')
-    parser_a = subparsers.add_parser('cert', help='Query a certificate (id, sha1, sha256 or serial)')
-    parser_a.add_argument('VALUE', help='Value to be requested, can be a crt.sh id, sha1, sha256 or serial')
-    parser_a.set_defaults(which='cert')
-    parser_b = subparsers.add_parser('domain', help='List certs related to a domain')
-    parser_b.add_argument('DOMAIN', help='domain')
-    parser_b.set_defaults(which='domain')
-    parser_b.add_argument('--caid', action='store', dest='caid', help='Define with which CA ID the search should be narrowed', default=None)
-    parser_b.add_argument('--exclude', action='store', dest='exclude', help='Define wich certificates should be excluded, for now only the keyword "expired" is supported', default=None)
+    """Provide CLI Arguments and prints data."""
+    parser = argparse.ArgumentParser(description="Request crt.sh")
+    subparsers = parser.add_subparsers(help="Commands")
+    parser_a = subparsers.add_parser(
+        "cert", help="Query a certificate (id, sha1, sha256 or serial)"
+    )
+    parser_a.add_argument(
+        "VALUE",
+        help="Value to be requested, can be a crt.sh id, sha1, sha256 or serial",
+    )
+    parser_a.set_defaults(which="cert")
+    parser_b = subparsers.add_parser("domain", help="List certs related to a domain")
+    parser_b.add_argument("DOMAIN", help="domain")
+    parser_b.set_defaults(which="domain")
+    parser_b.add_argument(
+        "--caid",
+        action="store",
+        dest="caid",
+        help="Define with which CA ID the search should be narrowed",
+        default=None,
+    )
+    parser_b.add_argument(
+        "--exclude",
+        action="store",
+        dest="exclude",
+        help='Define wich certificates should be excluded, \
+                for now only the keyword "expired" is supported',
+        default=None,
+    )
     args = parser.parse_args()
 
-    if hasattr(args, 'which'):
+    if hasattr(args, "which"):
         crt = Crtsh()
-        if args.which == 'cert':
+        if args.which == "cert":
             types = {32: "serial", 40: "sha1", 64: "sha256"}
             try:
-                t = types[len(args.VALUE)]
+                ctype = types[len(args.VALUE)]
             except KeyError:
-                t = "id"
-            res = crt.get(args.VALUE, type=t)
+                ctype = "id"
+            res = crt.get(args.VALUE, ctype=ctype)
             print(json.dumps(res, sort_keys=True, indent=4, default=datetime_handler))
-        elif args.which == "domain":
+        if args.which == "domain":
             res = crt.search(args.DOMAIN, args.caid, args.exclude)
             if len(res) == 0:
                 print("No certificate found!")
@@ -46,22 +69,24 @@ def main():
                 "Not Before",
                 "Not After",
                 "SAN",
-                "CA"
+                "CA",
             ]
 
-            for r in res:
+            for obj in res:
                 row = [
-                    r["id"],
-                    r["logged_at"].isoformat(),
-                    r["not_before"].isoformat(),
-                    r["not_after"].isoformat(),
-                    r["name"],
-                    r["ca"]["name"]
+                    obj["id"],
+                    obj["logged_at"].isoformat(),
+                    obj["not_before"].isoformat(),
+                    obj["not_after"].isoformat(),
+                    obj["name"],
+                    obj["ca"]["name"],
                 ]
                 table.add_row(row)
 
-            return table
+            print(table)
+
         else:
             parser.print_help()
+
     else:
         parser.print_help()
